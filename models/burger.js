da43e7eb4efa909b4d28e9d20668033e6781b9c9
RawKitMan <cjvaughn@rocketmail.com>
2019-02-04 13:57:56 -0700

    Completed burger.js file.

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,26 @@
+//Import the ORM config file
 const orm = ("../config/orm.js");
 
-//Create code that calls the ORM Methods
+//Object containing all of the ORM methods to query the SQL database
+const burger = {
+    all: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    },
 
+    create: function(columns, values, cb){
+        orm.insertOne("burgers", columns, values, function(res){
+            cb(res);
+        });
+    },
 
+    update: function(columns, condition, cb){
+        orm.updateOne("burgers", columns, condition, function(res){
+            cb(res);
+        });
+    }
+};
 
+//Exporting to burgers_controller.js
 module.exports = burger;
