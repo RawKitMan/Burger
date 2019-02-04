79618a1af51ee03ea679fb7dd6a3e2d0463f5333
RawKitMan <cjvaughn@rocketmail.com>
2019-02-04 14:48:38 -0700

    Added all function/query to burger.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,4 +5,17 @@ const router = express.Router();
 
 //Create the get, put, update, and delete routes (as needed) using "router.get" etc.
 
+router.get("/", function(req,res){
+
+    //The root page will show all the burgers in the database
+    burger.all(function(data){
+        //Stores the data we receive as an object to pass to
+        //our index Handlebars page.
+        let dataObj = {
+            burgers: data
+        };
+        //SHOW THE THING
+        res.render("index", dataObj);
+    });
+})
 module.exports = router;
